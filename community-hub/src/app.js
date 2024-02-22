c19afc2ad57a23fc29fc2980aad14f5deb41bc24
jovid_18 <cshcho99@gmail.com>
2024-02-22 20:08:03 +0900

    2.5

--- community-hub/src/app.js
+++ community-hub/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/users.router.js';
+import PostsRouter from './routes/posts.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 const app = express();
@@ -9,7 +10,7 @@ const PORT = 3018;
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UsersRouter]);
+app.use('/api', [UsersRouter, PostsRouter]);
 
 app.use(ErrorHandlingMiddleware);
 app.listen(PORT, () => {
