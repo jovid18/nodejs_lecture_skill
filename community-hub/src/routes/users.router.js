439fb98de534f6e354a318e23488600983ed7761
jovid_18 <cshcho99@gmail.com>
2024-02-22 15:08:20 +0900

    2.2 post/sign-in

--- community-hub/src/routes/users.router.js
+++ community-hub/src/routes/users.router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 //post api
@@ -31,6 +32,24 @@ router.post('/sign-up', async (req, res, next) => {
     },
   });
   return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+});
+
+router.post('/sign-in', async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await prisma.users.findFirst({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    return res.status(401).json({ message: '존재하지 않는 이메일입니다.' });
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+  }
+  const token = jwt.sign({ userId: user.userId }, 'custom-secret-key');
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(200).json({ message: '로그인에 성공하였습니다.' });
 });
 
 export default router;
