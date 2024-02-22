86e9ed2feaecc00c45936999bd42a9760ec9ef3f
jovid_18 <cshcho99@gmail.com>
2024-02-22 19:30:27 +0900

    2.4 errormiddleware

--- community-hub/src/app.js
+++ community-hub/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routes/users.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
+import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 const app = express();
 const PORT = 3018;
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api', [UsersRouter]);
 
+app.use(ErrorHandlingMiddleware);
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
 });
--- community-hub/src/routes/users.router.js
+++ community-hub/src/routes/users.router.js
@@ -7,32 +7,36 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 //post api
 router.post('/sign-up', async (req, res, next) => {
-  const { email, password, name, age, gender, profileImage } = req.body;
-  const isExistUser = await prisma.users.findFirst({
-    where: {
-      email,
-    },
-  });
-  if (isExistUser) {
-    return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
+  try {
+    const { email, password, name, age, gender, profileImage } = req.body;
+    const isExistUser = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (isExistUser) {
+      return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.users.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const userInfo = await prisma.userInfos.create({
+      data: {
+        userId: user.userId,
+        name,
+        age,
+        gender: gender.toUpperCase(),
+        profileImage,
+      },
+    });
+    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (err) {
+    next(err);
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await prisma.users.create({
-    data: {
-      email,
-      password: hashedPassword,
-    },
-  });
-  const userInfo = await prisma.userInfos.create({
-    data: {
-      userId: user.userId,
-      name,
-      age,
-      gender: gender.toUpperCase(),
-      profileImage,
-    },
-  });
-  return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
 });
 
 router.post('/sign-in', async (req, res, next) => {
